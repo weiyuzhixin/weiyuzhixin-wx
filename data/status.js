fb0583a4d5057b4db13fd6f1d91d0ff1837619ca
RachelALin <845195925@qq.com>
2020-03-05 20:52:09 +0800

    详情完成数据未绑定

---
+++ data/status.js
@@ -0,0 +1,6 @@
+var status_database = [
+{}
+]
+module.exports = {
+  Status_database: status_database
+}
--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,12 +1,24 @@
-
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
   },
+  bindDateChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
+  },
+
+  bindTimeChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      time: e.detail.value
+    })
+  },
+ 
 
   /**
    * 生命周期函数--监听页面加载
