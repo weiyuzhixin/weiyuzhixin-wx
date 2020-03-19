882f4efa61ccc715d64b83166b0b930dfc0e3502
RachelALin <845195925@qq.com>
2020-03-19 20:11:40 +0800

    数据完成首页和order

--- data/product.js
+++ data/product.js
@@ -16,43 +16,93 @@ var product_database = [{
     "id": 1,
     "src": "https://ae01.alicdn.com/kf/Hecc3218de7fd4ee3baae9095254c227fX.jpeg",
     "name": "Kay",
-    "price": 393
+    "price": 393,
+    "type": "一级",
+    "intro": "我是XX学院会计学专业XX届毕业生XXX。四年的大学生活，我认真学习专业知识，专业成绩名列前茅。现已系统的学习并掌握了本专业所开设的课程，如初级会计学、财务会计、财务管理、成本会计、管理会计、审计学、统计学等相关课程，有了扎实的专业理论基础；同时，我自学了Photoshop软件、金蝶财务软件，从而提高了我的自学能力。",
+    "time": "",
+    "phonenumber": "",
+    "coupon": "暂无",
+    "order_money": "暂无",
+    "sta": "已预订"
   },
   {
     "id": 2,
     "src": "https://ae01.alicdn.com/kf/Hecc3218de7fd4ee3baae9095254c227fX.jpeg",
     "name": "Kany",
-    "price": 393
+    "price": 393,
+    "type": "一级",
+    "intro": "我是XX学院会计学专业XX届毕业生XXX。四年的大学生活，我认真学习专业知识，专业成绩名列前茅。现已系统的学习并掌握了本专业所开设的课程，如初级会计学、财务会计、财务管理、成本会计、管理会计、审计学、统计学等相关课程，有了扎实的专业理论基础；同时，我自学了Photoshop软件、金蝶财务软件，从而提高了我的自学能力。",
+    "time": "",
+    "phonenumber": "",
+    "coupon": "暂无",
+    "order_money": "暂无",
+    "sta": "已预订"
   },
   {
     "id": 3,
     "src": "https://ae01.alicdn.com/kf/Hecc3218de7fd4ee3baae9095254c227fX.jpeg",
     "name": "Kaye",
-    "price": 393
+    "price": 393,
+    "type": "一级",
+    "intro": "我是XX学院会计学专业XX届毕业生XXX。四年的大学生活，我认真学习专业知识，专业成绩名列前茅。现已系统的学习并掌握了本专业所开设的课程，如初级会计学、财务会计、财务管理、成本会计、管理会计、审计学、统计学等相关课程，有了扎实的专业理论基础；同时，我自学了Photoshop软件、金蝶财务软件，从而提高了我的自学能力。",
+    "time": "",
+    "phonenumber": "",
+    "coupon": "暂无",
+    "order_money": "暂无",
+    "sta": "已预订"
   },
   {
     "id": 4,
     "src": "https://ae01.alicdn.com/kf/Hecc3218de7fd4ee3baae9095254c227fX.jpeg",
     "name": "Kayqe",
-    "price": 393
+    "price": 393,
+    "type": "一级",
+    "intro": "我是XX学院会计学专业XX届毕业生XXX。四年的大学生活，我认真学习专业知识，专业成绩名列前茅。现已系统的学习并掌握了本专业所开设的课程，如初级会计学、财务会计、财务管理、成本会计、管理会计、审计学、统计学等相关课程，有了扎实的专业理论基础；同时，我自学了Photoshop软件、金蝶财务软件，从而提高了我的自学能力。",
+    "time": "",
+    "phonenumber": "",
+    "coupon": "暂无",
+    "order_money": "暂无",
+    "sta": "已预订"
   },
   {
     "id": 5,
     "src": "https://ae01.alicdn.com/kf/Hecc3218de7fd4ee3baae9095254c227fX.jpeg",
     "name": "iKay",
-    "price": 393
+    "price": 393,
+    "type": "一级",
+    "intro": "我是XX学院会计学专业XX届毕业生XXX。四年的大学生活，我认真学习专业知识，专业成绩名列前茅。现已系统的学习并掌握了本专业所开设的课程，如初级会计学、财务会计、财务管理、成本会计、管理会计、审计学、统计学等相关课程，有了扎实的专业理论基础；同时，我自学了Photoshop软件、金蝶财务软件，从而提高了我的自学能力。",
+    "time": "",
+    "phonenumber": "",
+    "coupon": "暂无",
+    "order_money": "暂无",
+    "sta": "已预订"
   },
   {
     "id": 6,
     "src": "https://ae01.alicdn.com/kf/Hecc3218de7fd4ee3baae9095254c227fX.jpeg",
     "name": "KayKay",
-    "price": 393
+    "price": 393,
+    "type": "一级",
+    "intro": "我是XX学院会计学专业XX届毕业生XXX。四年的大学生活，我认真学习专业知识，专业成绩名列前茅。现已系统的学习并掌握了本专业所开设的课程，如初级会计学、财务会计、财务管理、成本会计、管理会计、审计学、统计学等相关课程，有了扎实的专业理论基础；同时，我自学了Photoshop软件、金蝶财务软件，从而提高了我的自学能力。",
+    "time": "",
+    "phonenumber": "",
+    "coupon": "暂无",
+    "order_money": "暂无",
+    "sta": "已预订"
+
   },
   {
     "id": 7,
     "src": "https://ae01.alicdn.com/kf/Hecc3218de7fd4ee3baae9095254c227fX.jpeg",
     "name": "KayKay",
-    "price": 393
+    "price": 393,
+    "type": "一级",
+    "intro": "我是XX学院会计学专业XX届毕业生XXX。四年的大学生活，我认真学习专业知识，专业成绩名列前茅。现已系统的学习并掌握了本专业所开设的课程，如初级会计学、财务会计、财务管理、成本会计、管理会计、审计学、统计学等相关课程，有了扎实的专业理论基础；同时，我自学了Photoshop软件、金蝶财务软件，从而提高了我的自学能力。",
+    "time": "",
+    "phonenumber": "",
+    "coupon": "暂无",
+    "order_money": "暂无",
+    "sta": "已预订"
   }
 ]
 module.exports = {
--- data/status.js
+++ data/status.js
@@ -1,12 +1,20 @@
 var status_database = [
 {
   "id":0,
-  "name":AnnaTest,
+  "name":"AnnaTest",
   "time":1,
   "price":123,
   "stanum":0,
   "sta":"未付款"
-}
+},
+{
+    "id": 1,
+    "name": "AnnaTest",
+    "time": 1,
+    "price": 123,
+    "stanum": 0,
+    "sta": "未付款"
+  }
 ]
 module.exports = {
   Status_database: status_database
